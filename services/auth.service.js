138a074970d57129738037839d8d321e683f07a1
Paweł Korobczyński <pawel.korobczynski@gmail.com>
2021-04-07 21:17:39 +0200

    refactor authentication service, send back expiration time

--- services/auth.service.js
+++ services/auth.service.js
@@ -13,7 +13,7 @@ module.exports = {
 
             return getResponsePayload(SUCCESS, null, null);
         } catch {
-            return getResponsePayload(FAIL, REGISTER_FAIL, null)
+            return getResponsePayload(FAIL, REGISTER_FAIL, null);
         }
     },
     loginUser: async (body) => {
@@ -21,13 +21,14 @@ module.exports = {
             const user = await db.User.findOne({ username: body.username });
 
             if (await bcrypt.compare(body.password, user.password)) {
+                const expiresIn = `${ process.env.ACCESS_TOKEN_EXPIRATION_TIME ?? 300000 }`;
                 const accessToken = jwt.sign(
                     { _id: user._id, username: user.username },
                     process.env.ACCESS_TOKEN_SECRET,
-                    { expiresIn: `${ process.env.ACCESS_TOKEN_EXPIRATION_TIME ?? "5min" }` }
+                    { expiresIn: expiresIn }
                 );
 
-                return getResponsePayload(SUCCESS, null, { accessToken: accessToken });
+                return getResponsePayload(SUCCESS, null, { accessToken, expiresIn });
             } else {
                 return getResponsePayload(FAIL, LOGIN_FAIL, null);
             }
@@ -39,11 +40,12 @@ module.exports = {
         try {
             const authHeader = req.headers["authorization"];
             const token = authHeader?.split(" ")[1];
-            if (!token)
+            if (!token) {
                 return res.json(getResponsePayload(FAIL, AUTHORIZATION_FAIL, null));
+            }
         
             const result = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
+            
             req.user = result;
             next();
         } catch {
