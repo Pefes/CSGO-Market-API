abec695e5d695715c4c7e09bdc5ad9f7cdb226f3
Paweł Korobczyński <pawel.korobczynski@gmail.com>
2021-10-10 15:31:18 +0200

    add mongodb uri variable

--- database/createConnection.js
+++ database/createConnection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const mongoDbUrl = "mongodb://localhost/my_database";
+const mongoDbUri = process.env.MONGODB_URI || "mongodb://localhost/my_database";
 
 const createConnection = () => {
-    mongoose.connect(mongoDbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = mongoose.connection;
     
     console.log("[createConnection.js] Database connection has been created!");
